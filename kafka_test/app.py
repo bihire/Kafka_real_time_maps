8033a0aec1204c5dc3332ba926685cd174cb5afb
bihire <muhireboris@yahoo.fr>
2020-01-18 22:48:40 +0200

    add leaflet map with markers

--- kafka_test/app.py
+++ kafka_test/app.py
@@ -1,43 +1,23 @@
+from flask import Flask, render_template, Response
 from pykafka import KafkaClient
-import json
-from datetime import datetime
-import uuid
 
+def get_kafka_client():
+    return KafkaClient(hosts='localhost:9092')
 
-input_file = open('./data/bus1.json')
-json_array = json.load(input_file)
-coordinates = json_array['features'][0]['geometry']['coordinates']
+app = Flask(__name__)
 
-# Generate uuid
-def generate_uuid():
-    return uuid.uuid4()
+@app.route('/')
+def index():
+    return(render_template('index.html'))
 
-# Kafaka producer
-client = KafkaClient(hosts="localhost:9092")
+@app.route('/topic/<topic_name>')
+def get_messsages(topic_name):
+    client = get_kafka_client()
+    def events():
+        for i in client.topics[topic_name].get_simple_consumer():
+            yield 'data:{0}\n\n'.format(i.value.decode())
 
-topic = client.topics['test_topic']
-producer = topic.get_sync_producer()
+    return Response(events(), mimetype="text/event-stream")
 
-
-# Generate all coordinates
-def generate_coordinates(coordinates):
-    # new_coordinates = []
-    for coordinate in coordinates:
-        data = {}
-        data['busline'] = 201
-        data['key'] = str(data['busline']) + '_' + str(generate_uuid())
-        data['time_stamp'] = str(datetime.utcnow())
-        data['longitude'] = coordinate[0]
-        data['latitude'] = coordinate[1]
-        message = json.dumps(data)
-        producer.produce(message.encode('ascii'))
-        # new_coordinates.append(data)
-    # return new_coordinates
-
-
-generate_coordinates(coordinates)
-
-
-print('every thing works fine')
-
-
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
---
+++ kafka_test/producer3.py
@@ -0,0 +1,53 @@
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+import time
+
+
+input_file = open('./data/bus3.json')
+json_array = json.load(input_file)
+coordinates = json_array['features'][0]['geometry']['coordinates']
+
+# Generate uuid
+
+
+def generate_uuid():
+    return uuid.uuid4()
+
+
+# Kafaka producer
+client = KafkaClient(hosts="localhost:9092")
+
+topic = client.topics['test_kafka2']
+producer = topic.get_sync_producer()
+
+
+# Generate all coordinates
+def generate_coordinates(coordinates):
+    # new_coordinates = []
+    i = 0
+    while i < len(coordinates):
+        data = {}
+        data['busline'] = 203
+        data['key'] = str(data['busline']) + '_' + str(generate_uuid())
+        data['time_stamp'] = str(datetime.utcnow())
+        data['longitude'] = coordinates[i][0]
+        data['latitude'] = coordinates[i][1]
+        message = json.dumps(data)
+        producer.produce(message.encode('ascii'))
+        time.sleep(1)
+
+        # If buses reaches last coordinaates
+        if i == len(coordinates)-1:
+            coordinates = coordinates[::-1]
+            i = 0
+        else:
+            i += 1
+    # return new_coordinates
+
+
+generate_coordinates(coordinates)
+
+
+print('every thing works fine')
